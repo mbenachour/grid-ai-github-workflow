24557e29103c48c362e196919c2bed7c091f67b4
mbenachour <mohamed.benachour@gmail.com>
2021-08-26 21:55:32 -0700

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+
 grid login --username $GRID_USERNAME --key $GRID_API_KEY
+
+ssh-keygen -b 2048 -t rsa -q -N ""
+
+grid ssh-keys add tmp_key ~/.ssh/id_rsa.pub
+
+grid session ssh covid-ct
+
