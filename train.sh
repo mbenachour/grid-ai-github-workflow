d64b90dd1d1b141212d76f1154819dd6e9c9ce78
Your Name <you@example.com>
2021-08-26 22:16:12 -0700

    change train.sh

--- train.sh
+++ train.sh
@@ -4,8 +4,9 @@ grid login --username $GRID_USERNAME --key $GRID_API_KEY
 
 ssh-keygen -t ed25519 -C "mohamed@mohamedbenachour.net" -q -N ""
 
+ls -la ~/.ssh/
 
-grid ssh-keys add ct-key ~/.ssh/id_ed25519.pub
+# grid ssh-keys add ct-key ~/.ssh/id_ed25519.pub
 
-grid session ssh covid-ct
+# grid session ssh covid-ct
 
